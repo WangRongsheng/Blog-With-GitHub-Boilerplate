7e29c031034724090e1b6c5238d5501f85e3bcfa
coder-王荣胜 <fujingshusheng@gmail.com>
2020-04-02 10:19:35 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "王荣胜"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2020-03-18T16:51+08:00"
 author = "王荣胜"
 email = "603329354@qq.com"
 author_homepage = "https://sqdxwz.com"
